41df231a77e719f9ef965be372fb1f9a9ba8df9d
Sibel Koc <sibel.koc.pro@outlook.fr>
2019-12-02 09:52:26 +0100

    fin exercice quete react 05

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,33 @@ import React from 'react';
 import './style.css';
 
 
-const Contact= props =>{
-    console.log(props);
-    return(
-    <div className="Contact">
-    <img className="avatar" src={props.avatar}/>
-        <div className ="status">
-        <h3 className="name">{props.name}</h3>
-            <div className="status-text">
-            <span className={props.status? 'status-online' :'status-offline'}></span>
-            {props.status?'online': 'offline'}
+class Contact extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            online: false,
+        };
+        this.updateOnline= this.updateOnline.bind(this);
+        }
+    updateOnline(){
+        this.setState({online: this.props.status ? true : false})
+        
+    }
+    render(){
+        return(
+            <div className="Contact">
+            <img className="avatar" src={this.props.avatar}/>
+                <div className ="status">
+                <h3 className="name">{this.props.name}</h3>
+                    <div className="status-text">
+                    <span onClick = {this.updateOnline} className={this.state.online? 'status-online' :'status-offline'}></span>
+                    {this.state.online? 'online': 'offline'}
+                    </div>
+
+                </div>
             </div>
-        </div>
-    </div>
-    );
+            );   
+    }
 };
 
 export default Contact;
